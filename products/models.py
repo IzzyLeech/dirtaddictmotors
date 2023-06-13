ac88e59c68f0056c53e5db4828b363bc3c2c356c
IzzyLeech <ralphhunter46@gmail.com>
2023-06-13 13:42:41 +0000

    added model for bikes in product app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Bikes(models.Model):
+    STROKE_CHOICES = (
+        (2, '2-stroke'),
+        (4, '4-stroke'),
+    )
+
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    stroke = models.IntegerField(choices=STROKE_CHOICES)
+    engine_capacity = models.DecimalField(max_digits=5, decimal_places=2)
+    speed = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    # image
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
