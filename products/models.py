8b8ab5e0281d438e6d42b69461bfe99710956496
IzzyLeech <ralphhunter46@gmail.com>
2023-06-13 13:45:04 +0000

    added sku code to bikes model

--- products/models.py
+++ products/models.py
@@ -14,6 +14,7 @@ class Bikes(models.Model):
     speed = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField()
+    sku = models.CharField(max_length=254, null=True, blank=True)
     # image
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
