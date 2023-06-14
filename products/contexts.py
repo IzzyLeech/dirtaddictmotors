70c3646e74053a542ccf7ec0f194d506d6892886
IzzyLeech <ralphhunter46@gmail.com>
2023-06-14 13:55:27 +0000

    added nav-category with include folder and added context processors for objects in Bike model to render through

---
+++ products/contexts.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from .models import Bikes
+
+
+def nav_category_manufacturers(request):
+    manufacturers = Bikes.objects.values_list('manufacturer', flat=True).distinct()
+    context = {'manufacturers': manufacturers}
+    return context
+
+
+def nav_category_engine_capacity(request):
+    engine_capacitys = Bikes.objects.values_list('engine_capacity', flat=True).distinct()
+    context = {'engine_capacitys': engine_capacitys}
+    return context
+
+
+def nav_category_stroke(request):
+    strokes = Bikes.objects.values_list('stroke', flat=True).distinct()
+    context = {'strokes': strokes}
+    return context
