0b49673e7e31818af261a0cfdb70473e1917f77a
IzzyLeech <ralphhunter46@gmail.com>
2023-06-14 16:47:15 +0000

    added link to display all bike in nav bar, added content to product page, image not rendering

--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Bikes
 
 
 def products_display(request):
     """View to display products with filter option for certain option"""
 
-    context = {}
+    bikes = Bikes.objects.all()
+
+    context = {
+        'bikes': bikes,
+        }
     return render(request, 'products/products.html', context)
