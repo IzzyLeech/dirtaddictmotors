3339d26645d8bd3931883a3a0096a54b43774d75
IzzyLeech <ralphhunter46@gmail.com>
2023-06-24 14:44:36 +0000

    added logic in the checkout view that receieves the bag content

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
 
+import json
+
+from products.models import Bikes
+
 
 def checkout_view(request):
-    return render(request, 'checkout/checkout.html')
+    # Retrieve the bag data from the session
+    bag_json = request.session.get('bag', '{}')
+    bag = json.loads(bag_json)
+
+    # Get the list of items from the bag
+    items = []
+    for item in bag.values():
+        bike_id = item['bike']['id']
+        quantity = item['quantity']
+        engine_capacity = item['bike']['engine_capacity']
+
+        # Retrieve the bike instance from the database based on bike ID
+        bike = Bikes.objects.get(pk=bike_id)
+
+        # Create a dictionary for the item
+        item_data = {
+            'bike': bike,
+            'quantity': quantity,
+            'engine_capacity': engine_capacity,
+        }
+
+        # Add the item to the list
+        items.append(item_data)
+
+    # Pass the items to the template
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
