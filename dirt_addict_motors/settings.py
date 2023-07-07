d43ac4268911fc9aec023201aa266d0d25aed99b
IzzyLeech <ralphhunter46@gmail.com>
2023-07-07 19:30:14 +0000

    Fixed allauth problems

--- dirt_addict_motors/settings.py
+++ dirt_addict_motors/settings.py
@@ -20,8 +20,6 @@ if os.path.isfile("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +53,6 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-
     'crispy_forms',
 ]
 
@@ -76,7 +73,10 @@ ROOT_URLCONF = 'dirt_addict_motors.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
