b3995ae2903fcaa8013cc071cec1e452241ef2f3
IzzyLeech <ralphhunter46@gmail.com>
2023-07-17 15:19:11 +0000

    added image widget for bike form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Bikes
+from .widgets import CustomClearableFileInput
 
 
 class BikeForm(forms.ModelForm):
@@ -7,3 +8,6 @@ class BikeForm(forms.ModelForm):
     class Meta:
         model = Bikes
         fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
